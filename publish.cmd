94c470907dc9997229ed5f3fe5c4ef5cf6b7c61f
Ashutosh Tiwari <ashutosh.tiwari05@hotmail.com>
2019-06-02 17:02:19 +0530

    First upload
    Please refer to Default.html to configure code base to the QnA Maker services

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=ieqnachatbotservice-bot-Web-Deploy.pubxml -p:Password=n6rfBjQlwhJhBxSPZHbqxSMvSik5CG7CpTWXJZrcTY4tf48Wwlf2lfuiYfd2
+
